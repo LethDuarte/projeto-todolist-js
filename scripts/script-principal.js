68b6f9aa8fb75ac47a4d1d2950f90518fa6b4c87
LethDuarte <lethduarte@gmail.com>
2022-12-12 16:28:36 -0300

    editando função criarNota

--- scripts/script-principal.js
+++ scripts/script-principal.js
@@ -1,14 +1,34 @@
 var nota = document.getElementById('itexto').value;
 //var paragrafo = document.getElementsByClassName('itemarq');
+var arquivo = [];
 
 function criarNota(){
-    var nota = document.getElementById('itexto').value;
-    var paragrafo = document.getElementById('arquivo');
-    var arquivo = [];
+    var nota = document.getElementById('itexto');
+    //var paragrafo = document.getElementById('itemarq');
+    var elementoPai = document.getElementById('arquivo');
+    var novoParagrafo = document.createElement('p');
 
-    arquivo.push(nota);
+    arquivo.push(nota.value);
 
-    paragrafo.innerHTML = "passa nada" + arquivo[0];
+    for(let item of arquivo){
+        elementoPai.appendChild(novoParagrafo);
+        novoParagrafo.className = 'itemarq';
+        novoParagrafo.innerHTML = item;
+    }
+    
+
+    //paragrafo.innerHTML = arquivo;
 
     console.log(arquivo);
 }
+
+// function criarElemento(){
+//     var elementoPai = document.getElementById('arquivo');
+//     var novoParagrafo = document.createElement('p');
+
+//     for(i=0; i++; i<arquivo.length){
+//         var texto = document.createTextNode(arquivo[i]);
+//         elementoPai.appendChild(novoParagrafo);
+//         novoParagrafo.appendChild(texto);
+//     }
+// }
